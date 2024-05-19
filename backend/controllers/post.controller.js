cb9b15274ddc14ad1d21fc9ade8fc2980f61de8b
Sudhir Rai <sudhirrai128@gmail.com>
2024-05-19 15:24:58 +0530

    Fixed typos and added controller for like and comment on a post

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,7 @@
 import Post from "../models/post.model.js";
 import { v2 as Cloudinary } from "cloudinary";
 import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 export const createPost = async (req, res) => {
   const { text } = req.body;
@@ -40,10 +41,10 @@ export const deletePost = async (req, res) => {
 
   try {
     const post = await Post.findById(postId);
-    if (!post) return res.status(400).josn({ error: "Post not found" });
+    if (!post) return res.status(400).json({ error: "Post not found" });
 
     if (post.user.toString() !== userId.toString())
-      return res.status(400).josn({ error: "You can't delete this post." });
+      return res.status(400).json({ error: "You can't delete this post." });
 
     if (post.img) {
       const imageId = await post.img.split("/").pop().split(".")[0];
@@ -53,7 +54,88 @@ export const deletePost = async (req, res) => {
     await Post.findByIdAndDelete(postId);
     res.status(200).json({ message: "Post deleted." });
   } catch (error) {
-    console.log("Error in deletePost controller", error.message)
-    res.status(500).json({error: "Internal server error"})
+    console.log("Error in deletePost controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const likePost = async (req, res) => {
+  const { id: postId } = req.params;
+  const userId = req.user._id;
+
+  try {
+    const post = await Post.findById(postId);
+    if (!postId) return res.status(400).json({ error: "Post not found" });
+
+    // Check if post is already liked by this user
+    const isLiked = post.likes.includes(userId);
+
+    if (isLiked) {
+      // unlike a post
+      await Post.findByIdAndUpdate(postId, { $pull: { likes: userId } });
+      await User.findByIdAndUpdate(userId, { $pull: { likedPosts: postId } });
+
+      res.status(200).json({ message: "You unliked this post" });
+    } else {
+      // Like a post
+      post.likes.push(userId);
+      await User.findByIdAndUpdate(userId, { $push: { likedPosts: postId } });
+      post.save();
+
+      // send Notification
+      const newNotification = new Notification({
+        from: userId,
+        to: post.user,
+        type: "like",
+      });
+
+      await newNotification.save();
+      res.status(200).json({ message: "You liked this post" });
+    }
+  } catch (error) {
+    console.log("Error in likePost controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const commentOnPost = async (req, res) => {
+  const userId = req.user._id;
+  const { id: postId } = req.params;
+  const { text } = req.body;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) return res.status(400).json({ error: "Post not found" });
+
+    if (!text)
+      return res.status(400).json({ error: "A comment must have text" });
+
+    const commentPayload = {text,
+      user: userId
+    }
+
+    post.comments.push(commentPayload)
+   await post.save()
+
+    res.status(200).json({message: "you commented on this post", post})
+  } catch (error) {
+    console.log("Error in CommentOnPost controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getAllPosts = async (req, res) =>{
+      try {
+        const posts = await Post.find().sort({createdAt: -1}).populate({
+          path: "user",
+          select: '-password'
+        }).populate({
+          path:"comments.user",
+          select: "-password"
+        })
+
+        res.status(200).json(posts)
+      } catch (error) {
+        console.log("Error in getAllPosts controller", error.message)
+        res.status(500).json({error: "Internal server error"})
+      }
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { createPost, deletePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost, getAllPosts, likePost, } from "../controllers/post.controller.js";
 
 const router = Router()
 
+router.get('/all-posts', protectRoute, getAllPosts)
 router.post("/create", protectRoute, createPost)
 router.delete("/delete/:id", protectRoute, deletePost)
+router.post("/like/:id", protectRoute, likePost)
+router.post('/comment/:id', protectRoute,commentOnPost)
 
 export default router
